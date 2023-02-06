7fed4a8bdd64d385ab6c0eee877c600f29229844
karinakustanovich <99758112+karinakustanovich@users.noreply.github.com>
2023-02-06 21:00:19 +0300

    ok

---
+++ src/test/java/lesson01/MyFirstRun.java
@@ -0,0 +1,36 @@
+package lesson01;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstRun {
+    public static void main(String[] args) throws InterruptedException {
+
+        // Setup chromedriver
+        WebDriverManager.chromedriver().setup();
+
+        //Initialization of ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        //Navigate to URL
+        driver.get("https://google.com");
+
+        //Current URL
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+
+        //Page Sourse
+//        System.out.println("driver.getPageSource() = " + driver.getPageSource());
+
+        //Title
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+
+        //Navigate to different URL
+        driver.navigate().to("https://www.yandex.ru");
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
