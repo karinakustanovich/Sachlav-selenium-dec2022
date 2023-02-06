2810ce6385973c4367993a2f6ce60af77f9f95af
karinakustanovich <99758112+karinakustanovich@users.noreply.github.com>
2023-02-06 21:15:12 +0300

    ok

--- src/test/java/lesson01/MyFirstRun.java
+++ src/test/java/lesson01/MyFirstRun.java
@@ -30,6 +30,18 @@ public class MyFirstRun {
         //Navigate to different URL
         driver.navigate().to("https://www.yandex.ru");
 
+        //Go Back
+        driver.navigate().back();
+
+        //Go Forward
+        driver.navigate().forward();
+
+        //Refresh
+        driver.navigate().refresh();
+
+        //Get Title
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+
         Thread.sleep(5000);
         driver.quit();
     }
