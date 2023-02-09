48b9c37fd00a064e6784b31ed2168eafe938e24f
karinakustanovich <99758112+karinakustanovich@users.noreply.github.com>
2023-02-09 17:17:09 +0300

    ok

---
+++ src/test/java/lesson01/Homework.java
@@ -0,0 +1,35 @@
+package lesson01;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Homework {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://amazon.com");
+
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+
+        driver.navigate().to("https://www.walmart.com");
+
+        driver.navigate().back();
+
+        driver.navigate().forward();
+
+        driver.navigate().refresh();
+
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+
+        System.out.println("driver.getTitle() = " + driver.getTitle());
+
+
+
+    }
+}
